38bc2fb4b4bee920f2dac84133c1aeca7de51356
omar adel <43929906+omarezz332@users.noreply.github.com>
2026-02-28 22:48:01 +0200

    first commit

---
+++ src/main/java/com/example/criteriaeligibilitycheckerservice/CriteriaEligibilityCheckerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.criteriaeligibilitycheckerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriteriaEligibilityCheckerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriteriaEligibilityCheckerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/criteriaeligibilitycheckerservice/CriteriaEligibilityCheckerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.criteriaeligibilitycheckerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriteriaEligibilityCheckerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
